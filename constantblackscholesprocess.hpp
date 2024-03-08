6076f53c7f7759473d9fcb145cccf782f230d749
Tddon <td.donnais@gmail.com>
2024-03-08 11:35:44 +0100

    First changes

--- constantblackscholesprocess.hpp
+++ constantblackscholesprocess.hpp
@@ -5,7 +5,21 @@ namespace QuantLib {
 
     class ConstantBlackScholesProcess : public StochasticProcess1D {
 
-        // your implementation goes here
+    private:
+        double spot;
+        double rf_rate;
+        double dividend;
+        double volatility;
+
+    public:
+        constantblackscholesprocess();
+        constantblackscholesprocess(constantblackscholesprocess& process);
+        constantblackscholesprocess(double spot, double rf_rate, double dividend, double volatility);
+        ~constantblackscholesprocess();
+        double getSpot() const;
+        double getRf_rate() const;
+        double getDividend() const;
+        double getVolatility() const;
         
     };
 
