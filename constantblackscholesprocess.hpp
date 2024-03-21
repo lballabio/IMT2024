3e8a77c92dcd4ee08d01752afbb5516d2cb15e53
toon2411 <82869007+toon2411@users.noreply.github.com>
2024-03-21 16:56:07 +0100

    Update constantblackscholesprocess.hpp

--- constantblackscholesprocess.hpp
+++ constantblackscholesprocess.hpp
@@ -1,32 +1,35 @@
 
+// constantblackscholesprocess.hpp
+#ifndef CONSTANTBLACKSCHOLES_PROCESS_HPP
+#define CONSTANTBLACKSCHOLES_PROCESS_HPP
+
 #include <ql/stochasticprocess.hpp>
+#include <ql/processes/eulerdiscretization.hpp>
 
-namespace QuantLib {
+using namespace QuantLib;
 
-    class ConstantBlackScholesProcess : public StochasticProcess1D {
+class constantblackscholesprocess : public StochasticProcess1D {
+private:
+    double spot;
+    double rf_rate;
+    double dividend;
+    double volatility;
 
-    private:
-        double spot;
-        double rf_rate;
-        double dividend;
-        double volatility;
+public:
+    constantblackscholesprocess();
+    constantblackscholesprocess(const constantblackscholesprocess &process_ref);
+    constantblackscholesprocess(double spot1, double rf_rate1, double dividend1, double volatility1);
+    ~constantblackscholesprocess();
 
-    public:
-        constantblackscholesprocess();
-        constantblackscholesprocess(constantblackscholesprocess& process);
-        constantblackscholesprocess(double spot, double rf_rate, double dividend, double volatility);
-        ~constantblackscholesprocess();
-        double getSpot() const;
-        double getRf_rate() const;
-        double getDividend() const;
-        double getVolatility() const;
+    double getSpot() const;
+    double getRf_rate() const;
+    double getDividend() const;
+    double getVolatility() const;
 
-        Real x0() const;
-        Real drift(Time t, Real x) const;
-        Real diffusion(Time t, Real x) const;
-        Real apply(Real x0, Real dx) const;
-        
-    };
+    Real x0() const;
+    Real drift(Time t, Real x) const;
+    Real diffusion(Time t, Real x) const;
+    Real apply(Real x0, Real dx) const;
+};
 
-}
-
+#endif // CONSTANTBLACKSCHOLES_PROCESS_HPP
