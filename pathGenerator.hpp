f3413ff22da00458627d01f617e0e508f68c53e5
toon2411 <82869007+toon2411@users.noreply.github.com>
2024-03-22 12:42:33 +0100

    Update pathGenerator.hpp

--- pathGenerator.hpp
+++ pathGenerator.hpp
@@ -12,41 +12,41 @@ namespace QuantLib {
 
     template <class RNG = PseudoRandom, class S = Statistics>
     class pathGenerator{
-      public:
-
-      typedef
-      typename McSimulation<SingleVariate, RNG,S>::path_generator_type
-          path_generator_type;
-
-    auto getPathGenerator(TimeGrid grid,
-                          typename RNG::rsg_type generator,
-                          boost::shared_ptr<StochasticProcess> process_,
-                          bool brownianBridge_,
-                          double strike,
-                          bool _constantParameters) {
-      // Here we distinguish both cases. At first, constant case
-         if (_constantParameters) {
-            
-            ext::shared_ptr <GeneralizedBlackScholesProcess> blackSC_process =
-                ext::dynamic_pointer_cast<GeneralizedBlackScholesProcess>(this->process_);
-            // Extracting the parameters here
-            Time time = grid.back();
-            double const_div = blackSC_process->dividendYield()->zeroRate(time, Continuous, NoFrequency);
-            double const_rf = blackSC_process->riskFreeRate()->zeroRate(time, Continuous, NoFrequency);
-            double const_volatility_ = blackSC_process->blackVolatility()->blackVol(time, strike);
-            double spot = blackSC_process->x0();
-
-            // Creating the constant BS Process
-            ext::shared_ptr <constantblackscholesprocess> const_blackSC_process(new constantblackscholesprocess(spot, const_rf, const_div, const_volatility_));
-            return ext::shared_ptr<path_generator_type>(
-                // Generator path with constant BS
-                new path_generator_type(const_blackSC_process, grid,generator,MCVanillaEngine<SingleVariate, RNG, S>::brownianBridge_));
-            } else {
+          public:
+    
+          typedef
+          typename McSimulation<SingleVariate, RNG,S>::path_generator_type
+              path_generator_type;
+    
+        auto getPathGenerator(TimeGrid grid,
+                              typename RNG::rsg_type generator,
+                              boost::shared_ptr<StochasticProcess> process_,
+                              bool brownianBridge_,
+                              double strike,
+                              bool _constantParameters) {
+          // Here we distinguish both cases. At first, constant case
+             if (_constantParameters) {
+                
+                ext::shared_ptr <GeneralizedBlackScholesProcess> blackSC_process =
+                    ext::dynamic_pointer_cast<GeneralizedBlackScholesProcess>(this->process_);
+                // Extracting the parameters here
+                Time time = grid.back();
+                double const_div = blackSC_process->dividendYield()->zeroRate(time, Continuous, NoFrequency);
+                double const_rf = blackSC_process->riskFreeRate()->zeroRate(time, Continuous, NoFrequency);
+                double const_volatility_ = blackSC_process->blackVolatility()->blackVol(time, strike);
+                double spot = blackSC_process->x0();
+    
+                // Creating the constant BS Process
+                ext::shared_ptr <constantblackscholesprocess> const_blackSC_process(new constantblackscholesprocess(spot, const_rf, const_div, const_volatility_));
                 return ext::shared_ptr<path_generator_type>(
-                            new path_generator_type(blackSCprocess_,
-                                        grid, generator, brownianBridge_));
-            }
-        }        
-    };
+                    // Generator path with constant BS
+                    new path_generator_type(const_blackSC_process, grid,generator,MCVanillaEngine<SingleVariate, RNG, S>::brownianBridge_));
+                } else {
+                    return ext::shared_ptr<path_generator_type>(
+                                new path_generator_type(blackSCprocess_,
+                                            grid, generator, brownianBridge_));
+                }
+            }        
+        };
 }
 
